4ad684ebcd76dbe2c10866dde750b8e7d6a02040
Matias Aguirre <matiasezeaguirre@gmail.com>
2024-09-17 19:24:07 -0300

    cambios en las migraciones

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/api/auth', authRoutes)
 
 app.use(errorMiddleware)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
